a1da24983702d08775ea97e1e435379ad3f1efbf
Your Namedantakelly <you@example.com>
2024-09-19 13:23:02 -0500

    ssr commit

--- client/src/components/accesspage/accesspage.jsx
+++ client/src/components/accesspage/accesspage.jsx
@@ -29,7 +29,8 @@ export default function AccessPage() {
             if (accessresponse.ok) {
                 const responseData = await accessresponse.json();
                 console.log(responseData.message); // Logs the success message
-                router.push('/dashboard'); // Redirect to dashboard
+                // router.push('/dashboard'); // Redirect to dashboard 
+                router.push('https://slowsleeprecords-client.vercel.app/dashboard');
             } else {
                 const errorData = await accessresponse.json();
                 setError(errorData.error || 'Server Error');
