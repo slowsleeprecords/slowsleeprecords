e13dfbbb1cb01a00e4cc89b1d7df45cfdf512bb5
Your Namedantakelly <you@example.com>
2024-09-18 22:45:19 -0500

    the ssr commit

--- server/prisma.js
+++ server/prisma.js
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL
+      url: process.env.POSTGRES_URL
     }
   }
 });
